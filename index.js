0872ac092b3b0348a4f69343d7b7e872e13e990f
anythingzen <chengzhanxd@gmail.com>
2024-06-05 01:35:52 +0800

    Edited javascript to allow players interaction for tictactoe.

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
 function gameBoard () {
-
-}
+    let board = [[], 
+                 [], 
+                 []
+                ]
+    return board;
+}   
 
 function tictactoe () {
 
-// board is 2d array, meaning 3 rows 3 cols
-    let board = [[],
-                 [], 
-                 []];
-// when user places, log down the row & column its placed
-const player1 = new Player()
-const player2 = new Player()
-// it then checks logic to see if it works.
+    const board = gameBoard();
+    const player1 = new Player('Player1', 'O');
+    const player2 = new Player('Player2', 'X');
+
+    let currentPlayer = player1;
+    
+    function moveDisplay () {
+
+        let gameboardDisplay = document.querySelector('.gameboard');
+        let row = 0;
+        let col = 0;
+
+        gameboardDisplay.addEventListener('click', (e) => {
+            const target = e.target;
+            row = parseInt(target.dataset.row);
+            col = parseInt(target.dataset.col);
+
+            if (!target.innerHTML) {
+                target.innerHTML  = currentPlayer.symbol;
+                board[row][col] = currentPlayer.symbol;
+
+                const result = logic(board, row, col, currentPlayer.symbol);
+
+                if (result) {
+                    alert(`Player: ${currentPlayer.name} has won!`);
+
+                }
+                else {
+                    currentPlayer = (currentPlayer === player1) ? player2 : player1;
+                    let flag = true;
+                    for (let row of board) {
+                        console.log(row)
+                        for (let cell of row) {
+                            console.log('cell: '+ cell)
+                            if (cell === undefined) {
+                                console.log(cell)
+                                flag = false;
+                            }
+                            break;
+                        }
+                        if (!flag) break;
+                    }
+                    if (flag) {
+                        alert('Tie!');
+                    }
+                }
+            }
+
+        })
+    }
+    moveDisplay();
 
 }
-// when box is pressed, 1. show O/X 
-// 2. log as first players value. Moves then alternates between X and O
-// player object is created to store the row and col
-// use each of the object to update gameBoard (prototype) -- if gameboard already has logged value, dont allow user to replace
-// check logic
-// display win 
-const Player = function (row, col) {
-    this.row = row;
-    this.col = col;
+
+const Player = function (name, symbol) {
+    this.name = name;
+    this.symbol = symbol;
 }
 
 // logic should comprise of 3 diff variants : 
@@ -32,12 +74,24 @@ function logic (board, rowPlaced, colPlaced, playerMove) {
     // horizontal checks - check only that row placed
     // if whole row same = win.
     const boardHorizontal = board[rowPlaced];
-    const horCheck = boardHorizontal.every(rowMove => rowMove === playerMove)
+    let horCheck = true;
+    for (let i = 0; i < 3; i++) {
+        if (boardHorizontal[i] !== playerMove) {
+            horCheck = false; 
+            break;
+        }
+    }
 
     // vertical checks - check only that vert placed
     // loop row, freeze colplaced
-    const vertCheck = board.every(vertMove => board[vertMove][colPlaced] === playerMove)
-
+    let vertCheck = true;
+    for (let i = 0; i < 3; i++) {
+        if (board[i][colPlaced] !== playerMove) { 
+            vertCheck = false; 
+            break;
+        }
+    }
+    
     // diagonal checks - check only the diagonal placed
     const diagonals = [[board[0][0], board[1][1], board[2][2]], 
                       [board[0][2], board[1][1], board[2][0]]]
@@ -48,5 +102,7 @@ function logic (board, rowPlaced, colPlaced, playerMove) {
                             diagonal.every(boardMove => boardMove === playerMove)
                         )
 
-    return (horCheck || vertCheck || diagonalCheck) ? 0 : 1;
+    return (horCheck || vertCheck || diagonalCheck);
 }
+
+tictactoe();
